3adad5a8f75d319c233a86b13ca45ca957f06aa5
Administrador <Administrador@10.10.4.3>
2019-09-26 16:47:48 -0300

    versao1111

---
+++ JavaApplication1/src/dao/DaoSacole.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Sacole;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Date;
+import java.time.format.DateTimeFormatter;
+/**
+ *
+ * @author Administrador
+ */
+public class DaoSacole {
+     public static boolean inserir(Sacole objeto) {
+        String sql = "INSERT INTO sacole (nr_serie, preco, sabor, data_validade) VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, objeto.getNr_serie());
+            ps.setDouble(2, objeto.getPreco());
+            ps.setString(3, objeto.getSabor());
+            ps.setDate(4, Date.valueOf(objeto.getData_validade()));
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+     public static void main(String[] args) {
+        Sacole objeto = new Sacole();
+        objeto.setNr_serie(22);
+        objeto.setPreco(22.6);
+        objeto.setSabor("BR");
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+     public static boolean alterar(Sacole objeto) {
+        String sql = "UPDATE sacole SET nr_serie = ?, preco = ?, sabor = ?, data_validade = ? WHERE codigo=?";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, objeto.getNr_serie());
+            ps.setDouble(2, objeto.getPreco());
+            ps.setString(3, objeto.getSabor());
+            ps.setInt(4, objeto.getCodigo());
+            ps.setDate(5, Date.valueOf(objeto.getData_validade()));
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+     public static boolean excluir(Sacole objeto) {
+        String sql = "DELETE FROM sacole WHERE codigo=?";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, objeto.getCodigo());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+     public static List<Sacole> consultar() {
+        List<Sacole> resultados = new ArrayList<>();
+        //editar o SQL conforme a entidade
+        String sql = "SELECT codigo, nr_serie, preco, sabor, data_validade FROM sacole";
+        PreparedStatement ps;
+        try {
+            ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Sacole objeto = new Sacole();
+                //definir um set para cada atributo da entidade, cuidado com o tipo
+                objeto.setCodigo(rs.getInt("codigo"));
+                objeto.setSabor(rs.getString("sabor"));
+                objeto.setNr_serie(rs.getInt("nr_serie"));
+                objeto.setNr_serie(rs.getInt("preco"));
+                objeto.setNr_serie(rs.getInt("data_validade"));
+                
+                
+                resultados.add(objeto);//não mexa nesse, ele adiciona o objeto na lista
+            }
+            return resultados;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+}
+      public static Sacole consultar(int primaryKey) {
+        //editar o SQL conforme a entidade
+        String sql = "SELECT codigo, sabor, nr_serie, preco, data_validade FROM sacole WHERE codigo=?";
+        PreparedStatement ps;
+        try {
+            ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, primaryKey);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Sacole objeto = new Sacole();
+                //definir um set para cada atributo da entidade, cuidado com o tipo
+                objeto.setCodigo(rs.getInt("codigo"));
+                objeto.setSabor(rs.getString("sabor"));
+                objeto.setNr_serie(rs.getInt("nr_serie"));
+                objeto.setPreco(rs.getDouble("preco"));
+                objeto.setData_validade(rs.getDate(44/88/2011));
+                return objeto;//não mexa nesse, ele adiciona o objeto na lista
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+}
